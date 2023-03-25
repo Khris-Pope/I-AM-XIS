68225e1388dc70395e1dd34f48b35d75a465c3d2
Khris-Pope <110688538+Khris-Pope@users.noreply.github.com>
2023-03-25 01:16:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ selectElement('.mobile-menu').addEventListener('click', () => {
    selectElement('header').classList.toggle('active');
 });
 
+/* ACCORDION */
+var acc =document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+   acc[i].addEventListener("click", function (){
+      this.classList.toggle("opened");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+         panel.style.maxHeight = null;
+      } else{
+         panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+   });
+}
+
 
 // const navigationHeight = document.querySelector('header').offsetHeight;
 
